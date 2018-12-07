fe09830662df0a1ae52a996b4bb8459997c9a321
RamSinghAKG <ram.singh.akg@gmail.com>
2018-12-07 23:29:34 +0530

    Rename to test page

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import OtherPage from './OtherPage';
+import TestPage from './TestPage';
 import Fib from './Fib';
 
 class App extends Component {
@@ -14,12 +14,12 @@ class App extends Component {
             <img src={logo} className="App-logo" alt="logo" />
             <h1 className="App-title">Fib Calculator version 1</h1>
             <Link to="/">Home</Link>
-            <Link to="/otherpage">Other Page</Link>
+            <Link to="/test">Test Page</Link>
           </header>
         
           <div>
             <Route exact path="/" component={Fib}></Route>
-            <Route exact path="/otherpage" component={OtherPage}></Route>
+            <Route exact path="/test" component={TestPage}></Route>
           </div>
         </div>
       </Router>
